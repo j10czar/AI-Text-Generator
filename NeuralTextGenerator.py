63c93178c01509dc4302a91111616ec5bb1a2d84
Jason Tenczar <jasontenczar@gmail.com>
2022-10-10 16:14:08 -0400

    fixed dir issues

---
+++ NeuralTextGenerator.py
@@ -0,0 +1,220 @@
+from asyncore import read
+from cgitb import text
+import random
+from tracemalloc import start
+import re
+import os
+
+
+
+#change these before running
+#modify project dir in order for the program to read in text files
+wordTree = {}
+blockedWords = []
+fillerWords = []
+# project_dir = r'C:\Users\jasea\VSCodeRepos\Basic-AI-Text-Generator'
+project_dir = os.path.dirname(os.path.realpath(__file__))
+
+#adds words into word tree from w 
+def populateTree(w):
+    for word in w:
+        wordTree[word] = []             
+
+
+#returns an arry of all unique words
+def unique(w):
+    uniqueWords = []
+    for word in w:
+        if word not in uniqueWords:
+            uniqueWords.append(word)
+
+    return uniqueWords
+
+#makes words easier to process
+def filterWord(word):
+    newWord = ''
+
+
+    for w in word:
+        if w!= '¶' and w!='(' and w!=')' and w!='.' and w!='*' and w!='“' and w!='”' and w!=',' and w!=':' and w!=';' and w!='"' and w!='-':
+            newWord += w
+    return newWord.lower().strip()     
+
+
+
+
+#filters the words to not be a single charater
+def filter(words):
+
+    filtered = []
+    for word in words:
+        word.replace('\u200b','')
+        if word != '' and word != '-' and word not in blockedWords and not word.isdigit():
+            filtered.append(filterWord(word))
+
+    return filtered        
+
+#returns an array of possible words that follow a word in a given text
+def matchWords(word, wordList):
+    matchedWords = []
+    occurrences = []
+
+    for index, elem in enumerate(wordList):
+        if elem == word:
+            occurrences.append(index)
+     
+
+    for i in occurrences:
+        if i >= len(wordList)-2:
+            i = len(wordList)-3
+
+        matchedWords.append(wordList[i+1])
+
+    return matchedWords    
+
+
+
+#populates tree with occurrences array
+def populateWords(wordList, wordTree):
+    for key in wordTree:
+        wordTree[key] = matchWords(key, wordList)
+
+
+#runs the simulatoin
+def runSimulation(wordTree,sortedWords,wordNum):
+    output = ''
+    if wordNum == 'sentence':
+        sentenceNum = random.randint(10,18)
+        text = ''
+        lastWord = ''
+        startIndex = random.randint(0, len(sortedWords)-2)
+
+        lastWord=sortedWords[startIndex]
+        text+=lastWord[0:1].upper() + lastWord[1:len(lastWord)] +' '
+
+        for i in range(0,sentenceNum):
+            arr = wordTree[lastWord]
+            lastWord = arr[random.randint(0, len(arr)-1)]
+            if i==sentenceNum-1:
+                text+= lastWord 
+            else:
+                text+= lastWord + ' '
+        print(text+'.')        
+    else:
+        while(wordNum>=5):
+            sentenceNum = random.randint(10,18)
+            text = ''
+            lastWord = ''
+            startIndex = random.randint(0,len(sortedWords)-2)
+
+            lastWord=sortedWords[startIndex]
+            text+=lastWord[0:1].upper() + lastWord[1:len(lastWord)] +' '
+
+            for i in range(0,sentenceNum):
+                arr = wordTree[lastWord]
+                lastWord = arr[random.randint(0, len(arr)-1)]
+                if i==sentenceNum-1:
+                    text+= lastWord
+                else:
+                    text+= lastWord + ' '
+            output+=text+'. '
+            wordNum -= sentenceNum
+        print(output)
+#intro sequence
+print("Text generation nerual net")
+print("By Jason Tenzcar")
+print('-----------')
+print("What file would you like to generate text from?")
+print('type "multiple" to generate a hybrid from 2 files')
+print("")
+print("")
+
+
+#reads in all folders in project dir
+folder_dir = os.listdir(os.path.expanduser(project_dir))
+
+txt_files = []
+
+for file in folder_dir:
+    if file[len(file)-4:] == '.txt':
+        txt_files.append(file)
+
+print_num = 1
+for file in txt_files:
+    print(str(print_num)+'- '+file)
+    print_num+=1
+
+print("")
+print("")    
+#takes and processes user input
+while(True):
+    user_choice = input('Type a number to choose or type "hybrid" for hybrid generation between 2 files')
+    print("")
+    if user_choice.isdigit() and int(user_choice)-1<=len(txt_files):
+        #opens text file(s) for reading and puts them into a useable string
+        textInput = txt_files[int(user_choice)-1]
+        print('generating word tree from '+str(textInput)+'...')
+        file = open(textInput,'r')
+        inputText = filter(re.split(' |\n', file.read())) 
+        break
+    elif user_choice=='hybrid':
+
+        while True:
+            optionOne = input("What is the first file you would like to use?")
+            if optionOne.isdigit() and int(optionOne)-1<=len(txt_files):
+                break
+            else:
+                print('invalid input')
+        while True:
+            optionTwo = input("What is the second file you would like to use?")
+            if optionTwo.isdigit() and int(optionTwo)-1<=len(txt_files):
+                break
+            else:
+                print('invalid input')
+        #opens text file(s) for reading and puts them into a useable string
+        textOne = txt_files[int(optionOne)-1]
+        textTwo = txt_files[int(optionTwo)-1]
+        print('You are now on hybrid mode...')
+        print('generating word tree from '+str(textOne)+' and '+str(textTwo)+'...')
+        fileOne = open(textOne,'r')
+        fileTwo = open(textTwo,'r')
+        inputText = filter(re.split(' |\n', fileOne.read())) + filter(re.split(' |\n', fileTwo.read()))
+        break
+    else:
+        print('invalid input')        
+#opens text file(s) for reading and puts them into a useable string
+
+
+
+sortedText = unique(inputText)
+sortedText.sort()
+
+
+
+#populates wordTree with corresponding words
+print('populating tree with unique words...')
+populateTree(sortedText)
+populateWords(inputText, wordTree)
+print('tree has been populated with '+str(len(sortedText))+' unique words')
+
+
+
+
+
+running = True
+while(running):
+    userInput = input('Press enter to generate sentence or type number of words you want in generation.')
+    if userInput == '':
+        print('__________________')
+        runSimulation(wordTree,sortedText,'sentence')
+        print('__________________')
+    elif userInput == 'e':
+        running = False
+
+    else:
+        if userInput.isdigit():
+            print('__________________')
+            runSimulation(wordTree,sortedText,int(userInput)-1)
+            print('__________________')
+        else:
+            print('please type a number or "e" to exit')
